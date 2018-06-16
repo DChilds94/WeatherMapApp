a839fa32e9f4a9e6b4429d59cae938290c4d45c3
Daniel Childs <dc.1994@outlook.com>
2018-06-16 18:00:59 +0100

    added openWeatherMaps api

--- public/index.js
+++ public/index.js
@@ -1,7 +1,4 @@
-const app = function(){
-
-
-}
+// CREATE THE MAP
 
 const drawMap = function() {
   const mapDiv = document.getElementById("map");
@@ -11,4 +8,27 @@ const drawMap = function() {
   mainMap.addMarker(glasgow);
 }
 
-window.addEventListener('load', drawMap);
+
+
+
+const app = function(){
+  drawMap();
+  let url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ec48c9ac25974ff3592099ccf76d18db";
+  makeRequest(url, requestComplete);
+}
+
+const makeRequest = function(url, callBack){
+  let request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener('load', callBack)
+  request.send();
+}
+
+const requestComplete = function(){
+  if (this.status !== 200) return;
+  const jsonString = this.responseText;
+  const weather = JSON.parse(jsonString);
+  console.log(weather);
+}
+
+window.addEventListener('load', app);
