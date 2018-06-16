a50a4889e565d45676df2fc49df395a17120555d
Daniel Childs <dc.1994@outlook.com>
2018-06-16 17:15:58 +0100

    can add marker on click

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -1,7 +1,9 @@
 const MapWrapper = function(element, coords, zoom){
   const osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
   this.map = L.map(element).addLayer(osmLayer).setView(coords, zoom);
-  // L.marker(center).addTo(this.map);
+  this.map.on("click", function(event){
+    this.addMarker(event.latlng)
+  }.bind(this))
 }
 
 MapWrapper.prototype.addMarker = function (coords) {
