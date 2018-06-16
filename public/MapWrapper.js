0ab7902f2bbf662fa383c8980516d441b2f4c9f6
Daniel Childs <dc.1994@outlook.com>
2018-06-16 16:52:02 +0100

    add  layer to map

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -1,4 +1,5 @@
 const MapWrapper = function(element){
-  this.map = L.map(element);
+  const osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+  this.map = L.map(element).addLayer(osmLayer).setView([0, 0], 15);
 
 }
