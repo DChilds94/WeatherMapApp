05a069ecb83f25cc00cc712fc4333c63d705b51a
Daniel Childs <dc.1994@outlook.com>
2018-06-16 20:01:44 +0100

    can change location to london on click of a button

--- public/index.js
+++ public/index.js
@@ -1,16 +1,5 @@
-// CREATE THE MAP
 
-const drawMap = function() {
-  const mapDiv = document.getElementById("map");
-  const glasgow = [ 55.86515, -4.25763]
-  // const zoom = 15
-  const mainMap = new MapWrapper(mapDiv, glasgow, 15);
-  mainMap.addMarker(glasgow);
-}
-
-
-
-
+// CREATE THE APP
 const app = function(){
   drawMap();
   let url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ec48c9ac25974ff3592099ccf76d18db";
@@ -28,10 +17,35 @@ const requestComplete = function(){
   if (this.status !== 200) return;
   const jsonString = this.responseText;
   const weather = JSON.parse(jsonString);
-  console.log(weather)
   displayWeather(weather);
-  locationButton();
+
 }
+
+
+
+// CREATE THE MAP
+
+const drawMap = function() {
+  const mapDiv = document.getElementById("map");
+  const glasgow = [ 55.86515, -4.25763]
+  const mainMap = new MapWrapper(mapDiv, glasgow, 15);
+
+  mainMap.addMarker(glasgow);
+  londonButton(mainMap);
+}
+
+const londonButton = function(map) {
+  let londonButton = document.createElement('button');
+  londonButton.classList.add('button');
+  londonButton.innerText = "Take me to London";
+  document.body.appendChild(londonButton);
+  londonButton.addEventListener('click', function(){
+    map.moveMap([51.509865, -0.118092]);
+    console.log(this);
+  })
+}
+
+
 
 const displayWeather = function(weather){
   let weatherInfo = document.createElement('div');
@@ -42,13 +56,17 @@ const displayWeather = function(weather){
   document.body.appendChild(weatherInfo);
 }
 
-const locationButton = function(){
-  let locationButton = document.createElement('button');
-  locationButton.classList.add('button');
-  locationButton.innerText = "Show Current Location";
-  document.body.appendChild(locationButton);
+// const locationButton = function(map){
+//   let locationButton = document.createElement('button');
+//   locationButton.classList.add('button');
+//   locationButton.innerText = "Show Current Location";
+//   document.body.appendChild(locationButton);
+//   locationButton.addEventListener('click', function(){
+//   this.map.currentLocation();
+//   })
+// }
 
-}
+
 
 
 
